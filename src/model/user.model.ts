386b00453df9095385237a50433c6519b6724dec
thomsult <thomsult@gmail.com>
2023-02-13 19:51:40 +0100

    add Prisma-MangoDB

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,11 +1,12 @@
 import { ObjectId } from "mongodb";
 import User from "~/type/user";
 import  db  from "~/utils/db";
+import prisma from "~/utils/prisma";
 
 const collection  = db.collection('User')
 
 export const getUsers = async ()=>{
-  const users = await collection.find({}).toArray()
+  const users = await prisma.user.findMany()
    return users.map((el:any)=>{
 el.hashedPassword = undefined;
 return el
@@ -13,32 +14,32 @@ return el
 }
 
 export const getUserByEmail = async (email:string)=>{
-    
-  const query = { email: email };
-  
-  const user = await collection.findOne(query)
+ 
+  const user = await prisma.user.findFirst({
+    where: { email: email },
+  })
     return user
 }
 
 export const getUserById = async (id:any)=>{
+  const user = await prisma.user.findFirst({
+    where: { id:id},
+  })
 
-  const user = await collection.findOne({ _id: new ObjectId(id) })
-  console.log(user)
     return user
 }
 export const deleteUserByEmail = async (email:string)=>{
-      const query = { email: email };
-     const deleteUser = await collection.deleteOne(query);
-
-    return deleteUser
+  const deleteUser = await prisma.user.delete({
+    where: { email: email },
+  })
+return deleteUser
 }
 
-export const addUser = async (currentUser:User)=>{
-    const userToAdd = await collection.insertOne({
-      email:currentUser.email,
-      hashedPassword:currentUser.hashedPassword
-    });
-      return userToAdd
+export const addUser = async (currentUser:any)=>{
+  const userToAdd = await prisma.user.create({
+    data: currentUser,
+  })
+  return userToAdd
     
 }
 
